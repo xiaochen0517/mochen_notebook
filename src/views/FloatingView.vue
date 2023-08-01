71c0711cc32af2056086e67076ee9a8a1df20892
mochenya <xiaochen0517@qq.com>
2023-08-01 13:18:02 +0800

    浮动窗口与md编辑器配置

--- src/views/FloatingView.vue
+++ src/views/FloatingView.vue
@@ -1,5 +1,72 @@
 <template>
-  <div class="floating-view">
-    123
+  <div class="floating-view" @mousemove="mousemoveHandle" @mousedown="mousedownHandle" @mouseup="mouseupHandle"
+       @click="clickHandle">
+    <el-icon>
+      <Edit/>
+    </el-icon>
   </div>
 </template>
+
+<script setup lang="ts">
+  import {onMounted, ref} from 'vue'
+
+  import {Edit} from '@element-plus/icons-vue'
+
+  import {appWindow, LogicalSize, WebviewWindow} from '@tauri-apps/api/window'
+
+  const canDraggable = ref(false)
+
+  onMounted(async () => {
+    await appWindow.setSize(new LogicalSize(40, 40))
+  })
+
+  const mousedownHandle = (): void => {
+    canDraggable.value = true;
+  }
+
+  const mouseupHandle = (): void => {
+    canDraggable.value = false;
+  }
+
+  const mousemoveHandle = async (): Promise<void> => {
+    if (canDraggable.value) {
+      await appWindow.startDragging()
+    }
+  }
+
+  const clickHandle = async (): Promise<void> => {
+    let mainWindow = WebviewWindow.getByLabel('main')
+    // 如果主页面窗口不存在，则创建主窗口
+    if (mainWindow == null) {
+      new WebviewWindow('main', {url: '/#/'})
+      return
+    }
+    // 检查窗口是否小化
+    if (await mainWindow.isMinimized()) {
+      // 如果主页面窗口小化，则恢复主窗口
+      await mainWindow.unminimize();
+    }
+    // 如果主页面窗口存在，则设置主窗口为焦点
+    await mainWindow.setFocus();
+  }
+</script>
+
+<style scoped>
+  .floating-view {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    background-color: #00ADB5;
+    border: 1px solid #00ADB5A0;
+    border-radius: 50%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    user-select: none;
+
+    .el-icon {
+      width: 100%;
+      height: 100%;
+      font-size: 18px;
+      color: #EEEEEE;
+    }
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view">
+    <div ref="MdPreviewRef"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Editor from '@toast-ui/editor'
+  import '@toast-ui/editor/dist/toastui-editor.css'
+  import '@toast-ui/editor/dist/i18n/zh-cn'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+    data () {
+      return {
+        editor: null,
+        editorText: '# Hello, world!',
+      }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        this.editor = new Editor({
+          el: this.$refs.MdPreviewRef,
+          initialValue: this.editorText,
+          previewStyle: 'vertical',
+          height: '100%',
+          usageStatistics: false,
+          language: 'zh-CN',
+        })
+      })
+    }
   }
-}
 </script>
+
+<style scoped lang="less">
+  .home-view {
+    height: 100%;
+    box-sizing: border-box;
+
+    /deep/ .toastui-editor-defaultUI {
+      box-sizing: border-box;
+    }
+  }
+</style>
